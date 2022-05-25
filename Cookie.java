cb33f9be01377f4c82413741f1d2ec9d9fe4fea1
William <billdinh52@gmail.com>
2022-05-24 21:33:22 -0700

    implimented jswing, created the gui for the cookie, added an image, fiddled around with jswing for a bit

--- Cookie.java
+++ Cookie.java
@@ -1,13 +1,33 @@
+import java.awt.Color;
 
+import javax.swing.*;
+public class Cookie extends JFrame{
+    
+    JLabel counter; 
 
-/**
- * Cookie
- */
-public class Cookie {
     public static void main(String[] args) {
-        System.out.println("hi");
+        new Cookie();
     }
+    public Cookie(){
+        CookieCreation();
+        
+    }
+    public void CookieCreation(){
+        //make the window
+        JFrame window = new JFrame("the best form of cookie clicker");
+        window.setSize(1024,768);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //make the panel
+        JPanel cpanel = new JPanel();
+        cpanel.setBounds(100,220,200,200);
+        cpanel.setBackground(Color.LIGHT_GRAY);
 
+        window.add(cpanel);
+        //icon for cookie, using eyeeye.png
+        JButton cbutton = new JButton(new ImageIcon("eyeye.png"));
+        cpanel.add(cbutton);
 
+        window.setVisible(true);
+    }
     
 }
