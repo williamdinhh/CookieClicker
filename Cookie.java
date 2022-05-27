2929f17fd8a38860bba65f221c6e227b79ebca4d
William <billdinh52@gmail.com>
2022-05-26 20:22:52 -0700

    fixed coord generator, made a method for counter, cleaned up code, begun making the CPS method

--- Cookie.java
+++ Cookie.java
@@ -1,58 +1,86 @@
 import java.awt.Color;
 import javax.swing.*;
-
-import java.awt.*;
+import java.awt.*;  
+import java.awt.event.*;  
 
 public class Cookie extends JFrame{
     //for the future cookie counter, keep on standby
     private int counter; 
-    public static void main(String[] args) {
+    private JLabel cps;
+    private JLabel clabel;
+    private CHandler handler = new CHandler();
+    private boolean timing;
 
-        //find the coordiante position of the cursor, future use(?)
-        double x = 0.0;
-        double y = 0.0;
-        while((x != 0.0)&& (y != 0.0))
-        {
-            x = MouseInfo.getPointerInfo().getLocation().x;
-            y = MouseInfo.getPointerInfo().getLocation().y;
-        }
-        System.out.println(x + "," + y);
+    public static void main(String[] args) {
         new Cookie();
+        //coords for the pointer, use to find the spacing and size of panels
+        while(true)
+        {
+            System.out.println(MouseInfo.getPointerInfo().getLocation().x + "," + MouseInfo.getPointerInfo().getLocation().y);
+        }
+        
     }
     
-
     public Cookie(){
         CookieCreation();
-        
     }
 
-    public void CookieCreation(){
-
-        JLabel label = new JLabel("Cookies: ");
-        
+    public void CookieCreation(){        
         //make the window
         JFrame window = new JFrame("the best form of cookie clicker");
         window.setSize(1024,768);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //make the panel
+        window.setLayout(null);
+        
+        //make the panel, cookie one
         JPanel cpanel = new JPanel();
+        
+        //fix bounds later
         cpanel.setBounds(100,220,200,200);
         cpanel.setBackground(Color.WHITE);
-        
         window.add(cpanel);
-        //icon for cookie, using eyeeye.png
-        JButton cbutton = new JButton(new ImageIcon("eyeye.png"));
 
-        cpanel.add(label);
-        //Clicker counter
-        cbutton.addActionListener(ActionEvent ->{
-            counter++;
-            label.setText(counter + " cookies");
-        });
+        //add the counter panel
+        JPanel countPanel = new JPanel();
+
+        //change bounds later
+        countPanel.setBounds(100, 100, 200, 100);
+
+        //2x1 layout, one for the CPS, one for the cookies
+        countPanel.setLayout(new GridLayout(2,1));
+        window.add(countPanel);
+
+        //icon for cookie button, using eyeeye.png
+        ImageIcon eyeeye = new ImageIcon("eyeye.png");
+        JButton cbutton = new JButton();
+        cbutton.addActionListener(handler);
+        cbutton.setBorder(null);
+        cbutton.setIcon(eyeeye);
         cpanel.add(cbutton);
-        window.setVisible(true);
-    }
-    
+
+        //label for the counter
+        clabel = new JLabel(counter + " cookies");
+        countPanel.add(clabel);
         
-    
-}
+        //cps counter, add later
+        cps = new JLabel();
+        countPanel.add(cps);
+
+        window.setVisible(true);
+        
+    }
+    //use this method to calcualte CPS, for later.
+    public void cookieTimer(){
+
+    }
+    //way easier way of adding the cookie counter, using abstract classes
+    public class CHandler implements ActionListener{
+
+        public void actionPerformed(ActionEvent event){
+            counter++;
+            clabel.setText(counter + " cookies");
+
+        }
+    }
+
+    }
