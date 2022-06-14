f4bffddc539f7d69a919695fd592e677384784b4
William Dinh <103615278+williamdinhh@users.noreply.github.com>
2022-06-13 21:06:48 -0700

    Update CookieUI.java

--- CookieUI.java
+++ CookieUI.java
@@ -323,9 +323,8 @@ public class CookieUI {
 				}
 				
 			}
-            //two else if statements looks weird on github, but on my pc is fine. Probably since I copied and pasted the code above for convience
 			else if(button == upg3){
-                if(farmUnlocked == false){
+                		if(farmUnlocked == false){
 					popText.setText("You cannot purchase this yet");
 				}
 				else{
@@ -333,7 +332,7 @@ public class CookieUI {
 				}
 			}
 			else if(button == upg4){
-                if(mineUnlocked == false){
+                		if(mineUnlocked == false){
 					popText.setText("You cannot purchase this yet");
 				}
 				else{
